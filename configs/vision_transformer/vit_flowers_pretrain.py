b25a4069728867008fdf668669df363fe3fda365
SmartBea_Linlin_r <80329948+GaoRuilin1@users.noreply.github.com>
2024-04-24 22:07:39 +0800

    Update vit_flowers_pretrain.py

--- configs/vision_transformer/vit_flowers_pretrain.py
+++ configs/vision_transformer/vit_flowers_pretrain.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../_base_/models/vit_base_flowers.py',
     '../_base_/datasets/flowers_bs32.py',
-    '../_base_/schedules/flowers_pretrain_vit.py',
+    '../_base_/schedules/flowers_lrlow_pretrain_vit.py',
     '../_base_/tinyvit_save_runtime.py'
 ]
 
